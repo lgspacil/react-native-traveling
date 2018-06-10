24dfdcc185234662cb35690156cf36680d5fd197
Lucas Spacil <lucasspacil@Lucass-MacBook-Air.local>
2018-06-10 11:22:37 -0700

    inital commit

---
+++ src/store/reducers/places.js
@@ -0,0 +1,13 @@
+const initalState = {
+    places : [],
+    selectedPlace: null
+};
+
+const reducer = (state = initalState, action) => {
+    switch (action.type){
+        default:
+            return state;
+    }
+};
+
+export default reducer;
