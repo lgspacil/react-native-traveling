360676ce45f33fb704d7d779a85e692f663f839a
Lucas Spacil <lucasspacil@Lucass-MacBook-Air.local>
2018-06-14 18:47:27 -0700

    started tp added navigation

---
+++ index.android.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
+import configureStore from './src/store/configureStore';
+import App from './App';
+
+const store = configureStore();
+
+
+// Needed to make this a function since it was expecting one
+const RNRedux = () =>(
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+
+AppRegistry.registerComponent('awesomeplace', () => RNRedux);
